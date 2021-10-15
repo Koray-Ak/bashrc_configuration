ad247b9b9e6b8d264cafdbf55a0cfc8850e4b7a8
Koray Ak <koray.ak@gdi-service.de>
2021-10-15 14:25:59 +0300

    Created a new one

--- bash_history.sh
+++ bash_history.sh
@@ -1,42 +1,28 @@
 #!/bin/bash
 #This script is only for user gisadmin
 
-bashrc="/home/gisadmin/.bashrc"
-Red='\033[0;31m'
-Green='\033[0;32m'
-NC='\033[0m'
 
-#HISTCONTROL=ignoreboth has been changed HISTCONTROL=ignoredups
-HISTCONTROL="$(cat $bashrc | grep HISTCONTROL=)"
-sed -i s/HISTCONTROL=.*/HISTCONTROL=ignoredups/g $bashrc
-if [ "$HISTCONTROL" = "HISTCONTROL=ignoredups" ]
+ROOT_UID=0     # Only users with $UID 0 have root privileges.
+E_NOTROOT=87   # Non-root exit error.
+
+# Run as root, of course.
+if [ "$UID" -ne "$ROOT_UID" ]
 then
-	echo -e "${Green}$HISTCONTROL ${NC}already up-to-date"
-else
-	echo -e "${Red}$HISTCONTROL ${NC}has been changed ${Green}HISTCONTROL=ignoredups"
+  echo "Must be root to run this script."
+  exit $E_NOTROOT
 fi
 
-
-#HISTFILESIZE=2000 has been changed HISTFILESIZE=
-HISTFILESIZE="$(cat $bashrc | grep HISTFILESIZE=)"
-sed -i s/HISTFILESIZE=.*/HISTFILESIZE=/g $bashrc
-if [ "$HISTFILESIZE" = "HISTFILESIZE=" ]
-then
-	echo -e "${Green}$HISTFILESIZE ${NC}already up-to-date"
-else 
-	echo -e "${Red}$HISTFILESIZE ${NC}has been changed ${Green}HISTFILESIZE="
-fi
-
-
-# Insert a new format HISTTIMEFORMAT line 21
-string="HISTTIMEFORMAT=.*"
 bashrc="/home/gisadmin/.bashrc"
-HISTTIMEFORMAT="$(cat $bashrc | grep HISTTIMEFORMAT=)"
+	sed -i \
+	-e "s|HISTCONTROL=ignoreboth|HISTCONTROL=ignoredups|g" \
+	-e "s|HISTFILESIZE=2000|HISTFILESIZE=|g" \
+$bashrc
 
+#echo "HISTTIMEFORMAT=\"%F %T "\" >> $bashrc
+
+string="HISTTIMEFORMAT"
 if grep -q "$string" "$bashrc" ; then
-        sed -i 's/HISTTIMEFORMAT=.*/HISTTIMEFORMAT="%F %T "/g' $bashrc
-        echo -e "${Red}$HISTTIMEFORMAT ${NC}has been changed ${Green}HISTTIMEFORMAT=\"%F %T "\"""
+exit
 else
-        sed -i '21 i HISTTIMEFORMAT="%F %T "' $bashrc
-        echo -e "${NC}Insert a new format at line 21 ${Green}HISTTIMEFORMAT=\"%F %T "\"""
+echo "HISTTIMEFORMAT=\"%F %T "\" >> $bashrc
 fi
